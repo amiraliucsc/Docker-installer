624e348c9b765d1be8b56914c1caa246746c7178
Amirali <ashahinp@ucsc.edu>
2018-01-03 17:44:21 -0800

    Remove the old file, add the comments and README file

--- installer.sh
+++ installer.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# Uninstall the old version of the docker if happens to be install
 sudo apt-get remove docker docker-engine docker.io
+# Install necessary packages
 sudo apt-get update
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
+# Download and add the docker GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -14,7 +17,9 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+# Add the user to the docker group
 sudo groupadd docker
 sudo usermod -aG docker $USER
+# Install docker compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
